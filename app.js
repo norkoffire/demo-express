b7f299acaf2dc2fc35eead8ab30a3ae7e9b943ce
norkoffire <norkoffire@gmail.com>
2024-11-14 17:14:30 +0100

    add database connexion

--- app.js
+++ app.js
@@ -1,7 +1,28 @@
 const express = require("express");
+const mongoose = require("mongoose");
 const app = express();
+app.use(express.json());
+mongoose
+  .connect(
+    "mongodb+srv://norkoffire:AUe9hsOeaknclXpT@demo-express.1fl4y.mongodb.net/?retryWrites=true&w=majority&appName=demo-express"
+  )
+  .then(() => console.log("Connexion à MongoDB réussie !"))
+  .catch(() => console.log("Connexion à MongoDB échouée !"));
 
-app.use("/api/stuff", (req, res, next) => {
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content, Accept, Content-Type, Authorization"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PUT, DELETE, PATCH, OPTIONS"
+  );
+  next();
+});
+
+app.get("/api/stuff", (req, res, next) => {
   const stuff = [
     {
       _id: "oeihfzeoi",
@@ -23,6 +44,13 @@ app.use("/api/stuff", (req, res, next) => {
     },
   ];
   res.status(200).json(stuff);
+});
+
+app.post("/api/stuff", (req, res, next) => {
+  console.log(req.body);
+  res.status(201).json({
+    message: "Objet créé !",
+  });
 });
 
 module.exports = app;
